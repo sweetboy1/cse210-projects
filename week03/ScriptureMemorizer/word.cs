1e9d4d8bc1a642f86ccf3657b2320856f1474f39
sweetboy1 <44626732+sweetboy1@users.noreply.github.com>
2025-07-22 14:32:04 +0100

    Update word.cs

--- week03/ScriptureMemorizer/word.cs
+++ week03/ScriptureMemorizer/word.cs
@@ -1,1 +1,38 @@
+public class Word
+{
+    private string _text;
+    private bool _isHidden;
 
+    public Word(string text)
+    {
+        _text = text;
+        _isHidden = false;
+    }
+
+    public bool IsHidden()
+    {
+        return _isHidden;
+    }
+
+    public void Hide()
+    {
+        _isHidden = true;
+    }
+
+    public string GetDisplayText()
+    {
+        if (_isHidden)
+        {
+            char[] hidden = new char[_text.Length];
+            for (int i = 0; i < _text.Length; i++)
+            {
+                hidden[i] = Char.IsPunctuation(_text[i]) ? _text[i] : '_';
+            }
+            return new string(hidden);
+        }
+        else
+        {
+            return _text;
+        }
+    }
+}
