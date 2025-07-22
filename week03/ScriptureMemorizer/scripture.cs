2f99f7affdc8fff1010d8f26997dc02a58fa0b48
sweetboy1 <44626732+sweetboy1@users.noreply.github.com>
2025-07-22 14:23:37 +0100

    Update scripture.cs

--- week03/ScriptureMemorizer/scripture.cs
+++ week03/ScriptureMemorizer/scripture.cs
@@ -1,1 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(word => new Word(word)).ToList();
+    }
+
+    public void HideRandomWords(int count)
+    {
+        Random rand = new Random();
+        List<Word> visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+
+        for (int i = 0; i < count && visibleWords.Count > 0; i++)
+        {
+            int index = rand.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        return $"{_reference}\n{string.Join(" ", _words.Select(w => w.GetDisplayText()))}";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return _words.All(w => w.IsHidden());
+    }
+}
