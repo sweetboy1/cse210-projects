fa9d89255860b3e830df3119a82101f9f65a5b9a
sweetboy1 <44626732+sweetboy1@users.noreply.github.com>
2025-07-22 15:13:33 +0100

    Update video.cs

--- week04/YouTubeVideos/video.cs
+++ week04/YouTubeVideos/video.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Collections.Generic;
 
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int LengthInSeconds { get; set; }
+
+    private List<Comment> _comments = new List<Comment>();
+
+    public void AddComment(Comment comment)
+    {
+        _comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return _comments.Count;
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Title: {Title}");
+        Console.WriteLine($"Author: {Author}");
+        Console.WriteLine($"Length: {LengthInSeconds} seconds");
+        Console.WriteLine($"Comments ({GetCommentCount()}):");
+
+        foreach (var comment in _comments)
+        {
+            Console.WriteLine($" - {comment.CommenterName}: {comment.Text}");
+        }
+
+        Console.WriteLine(); // For spacing
+    }
+}
