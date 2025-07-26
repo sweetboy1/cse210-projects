ca109fba568fd159565e87650781483569010cfd
sweetboy1 <44626732+sweetboy1@users.noreply.github.com>
2025-07-26 22:51:54 +0100

    orders.cs

---
+++ week04/OnlineOrdering/orders.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        total += _customer.IsInUSA() ? 5 : 35;
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+        foreach (Product product in _products)
+        {
+            label += $"- {product.GetPackingLabel()}\n";
+        }
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress().GetFullAddress()}";
+    }
+}
